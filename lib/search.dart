ce1f42c81d92cd1b1573c67e086a2e1349504012
Amey <ameysunu00719@gmail.com>
2020-05-10 17:30:55 +0300

    fifth commit

--- lib/search.dart
+++ lib/search.dart
@@ -10,26 +10,53 @@ class Search extends StatelessWidget {
         body: SafeArea(
             child: Column(
               children: <Widget>[
-                 Card(                 //Card1
-                  color:Colors.grey[900],
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal:10.0),
-                  child:ListTile(
-                    leading: Icon(Icons.blur_circular ,
-                     color: Colors.white,
-                     ),
-                     title:  Text('Under Construction',
-                     style: TextStyle(color: Colors.white,
-                     fontSize: 20.0,
-                     ),
-                     ),
-                     subtitle: Text('We are still working on this tab :)',
-                     style: TextStyle(color: Colors.white,
-                     fontSize: 15.0,
-                     ),
-                     ),
-                  )
-                  ),
+                 Padding(
+                   padding: const EdgeInsets.all(16.0),
+                   child: TextFormField(
+                     style: new TextStyle(color:Colors.white),
+                   decoration: InputDecoration(
+                     prefixIcon: Icon(Icons.search,
+                     color: Colors.grey[600]),
+                   enabledBorder: new OutlineInputBorder(borderRadius: new BorderRadius.horizontal(),
                    
+                     borderSide: new BorderSide(
+                            color: Colors.white                            
+                          ),
+                        ),
+                   hintText: 'Search for a country/city',
+                   hintStyle: TextStyle(color: Colors.grey[600])
+                   ),
+                   ),
+                 ),
+                   
+                   Text("----------or----------",
+                   style: TextStyle(
+                     color: Colors.white,
+                   fontSize: 25.0),
+                   ),
+                   InkWell(
+                    child: Card(            
+                    color:Colors.grey[900],
+                    margin: EdgeInsets.symmetric(vertical: 12.0, horizontal:12.0),
+                    child:ListTile(
+                      leading: Icon(Icons.location_searching ,
+                       color: Colors.white,
+                       ),
+                        title: Text('get your location for updates',
+                        textAlign: TextAlign.center,
+                         style: TextStyle(color: Colors.white,
+                         fontSize: 20.0,
+                         
+                         ),
+                         ),
+                         onTap: ()async{
+                          
+                         },
+              
+                    ),
+                    ),
+                   )
+                     
               ]
               ),
         ),
